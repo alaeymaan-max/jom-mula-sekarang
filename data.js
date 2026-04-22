799c7c51a2086f5626bc54b5bbb1d820c524feda
alaeyman <alaeymaan@gmail.com>
2026-04-22 19:10:44 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+// Contoh struktur dalam data.js
+const guides = [
+  {
+    id: "masak-nasi-1",
+    category: "asas-hidup",
+    title: "Cara Masak Nasi Sempurna",
+    steps: [
+      { 
+        level: "mudah", 
+        text: "Basuh beras 2 kali sahaja.", 
+        image: "wash.jpg",
+        motivation: "Beras yang bersih menghasilkan nasi yang wangi!"
+      },
+      // ... langkah seterusnya
+    ],
+    donationLink: "https://ko-fi.com/jom-mula"
+  }
+];
